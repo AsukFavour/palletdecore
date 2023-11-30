844f54763665fa026786aa995c751939fbd2c9cc
favour asuk <favourasuk@gmail.com>
2023-11-30 09:16:22 +0100

    changes

--- src/component/GridLayout/Grid.jsx
+++ src/component/GridLayout/Grid.jsx
@@ -7,15 +7,15 @@ const Grid = () => {
     <div className="grid-container">
       <a href="path/to/link1" className="grid-item grid-item-large">
         <img src={image} alt="Image 1" />
-        <div className="image-text">Image 1</div>
+        <div className="image-text">Lounges</div>
       </a>
       <a href="path/to/link2" className="grid-item grid-item-small">
         <img src={image} alt="Image 2" />
-        <div className="image-text">Image 2</div>
+        <div className="image-text">Bedroom collection</div>
       </a>
       <a href="path/to/link3" className="grid-item grid-item-small">
         <img src={image} alt="Image 3" />
-        <div className="image-text">Image 3</div>
+        <div className="image-text">Dining collection</div>
       </a>
     </div>
   );
